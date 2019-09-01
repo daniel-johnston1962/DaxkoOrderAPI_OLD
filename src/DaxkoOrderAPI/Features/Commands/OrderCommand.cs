f52a3ac75592656c23c2ac3ad33903a3d7c89ea1
Dan Johnston <dan.johnston@outlook.com>
2019-09-01 10:26:34 -0500

    small change

--- src/DaxkoOrderAPI/Features/Commands/OrderCommand.cs
+++ src/DaxkoOrderAPI/Features/Commands/OrderCommand.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace DaxkoOrderAPI.Features.Commands
+﻿namespace DaxkoOrderAPI.Features.Commands
 {
     public class OrderCommand
     {
